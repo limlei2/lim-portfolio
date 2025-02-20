e74ec13bb1cb24709c54c8fafc0466664b7f4ce7
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-02-19 22:47:43 -0500

    skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Skills from "./components/Skills";
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
             <main>
                 <Hero />
                 <About />
+                <Skills />
             </main>
         </>
     )
---
+++ src/components/Skills.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import SkillCard from "./SkillCard";
+
+const skillItems = [
+  {
+    imgSrc: "/images/javascript.svg",
+    label: "JavaScript",
+    desc: "Coding Language"
+  },
+  {
+    imgSrc: "/images/css3.svg",
+    label: "CSS",
+    desc: "User Interface"
+  },
+  {
+    imgSrc: "/images/html.svg",
+    label: "HTML",
+    desc: "Web Design"
+  },
+  {
+    imgSrc: "/images/react.svg",
+    label: "React",
+    desc: "Frontend Framework"
+  },
+  {
+    imgSrc: "/images/expressjs.svg",
+    label: "ExpressJS",
+    desc: "Node Framework"
+  },
+  {
+    imgSrc: "/images/nodejs.svg",
+    label: "NodeJS",
+    desc: "Web Server"
+  },
+  {
+    imgSrc: "/images/mongodb.svg",
+    label: "MongoDB",
+    desc: "Database"
+  },
+  {
+    imgSrc: "/images/aws.svg",
+    label: "AWS",
+    desc: "Cloud Computing"
+  },
+  {
+    imgSrc: "/images/mysql-logo.svg",
+    label: "MySQL",
+    desc: "Database"
+  },
+  {
+    imgSrc: "/images/java.svg",
+    label: "Java",
+    desc: "Coding Language"
+  },
+  {
+    imgSrc: "/images/python.svg",
+    label: "Python",
+    desc: "Coding Language"
+  },
+  {
+    imgSrc: "/images/c-1.svg",
+    label: "C",
+    desc: "Coding Language"
+  },
+
+];
+
+const Skills = () => {
+  return (
+    <section className="section">
+      <div className="container">
+        <h2 className="headline-2">
+          Tools I Am Familiar With
+        </h2>
+
+        <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch]">
+          These are the tools I always use and am extremely fluent in to be able to create and design high-functioning webpages.
+        </p>
+
+        <div className="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]">
+          {
+            skillItems.map(({ imgSrc, label, desc }, key) => (
+              <SkillCard 
+                key={key}
+                imgSrc={imgSrc}
+                label={label}
+                desc={desc}
+              />
+            ))
+          }
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Skills
