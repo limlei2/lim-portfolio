b5ab7127eb9855384947ccf9ece56ce615946927
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-02-27 22:38:40 -0500

    8000

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ export default defineConfig({
  base: "/",
  plugins: [react()],
  preview: {
-  port: 8080,
+  port: 8000,
   strictPort: true,
  },
  server: {
-  port: 8080,
+  port: 8000,
   strictPort: true,
   host: true,
   origin: "http://0.0.0.0:8080",
